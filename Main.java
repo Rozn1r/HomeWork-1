8e8a42e1307265e316ac21263f9e4b77abcbdb0a
Rozn1r <perevertun13385@gmail.com>
2025-09-12 23:11:57 +0500

    HomeWork

--- Main.java
+++ Main.java
@@ -4,12 +4,84 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        var resulta = dog + 4;
+        System.out.println(dog + 4);
+        var resultb = cat + 4;
+        System.out.println(cat + 4);
+        var resultc = paper + 4;
+        System.out.println(paper + 4);
+        var resultx = dog - 3.5;
+        System.out.println(resulta - 3.5);
+        var resulty = cat - 1.6;
+        System.out.println(resultb - 1.6);
+        var resultz = paper - 7639;
+        System.out.println(resultc - 7639);
 
-        for (int i = 1; i <= 5; i++) {
+
+
+        var friend = 19;
+        System.out.println(friend);
+        var resultf = friend + 2;
+        System.out.println(friend + 2);
+        var resultfr = resultf / 7;
+        System.out.println(resultf / 7);
+
+
+
+        var frog = 3.5;
+        System.out.println(frog);
+        var result = frog * 10;
+        System.out.println(frog * 10);
+        var resultF = result / 3.5;
+        System.out.println(result / 3.5);
+        var resultFr = resultF + 4;
+        System.out.println(resultF + 4);
+
+
+
+        var massBoxer1 = 78.2;
+        var massBoxer2 = 82.7;
+        var totalMass = massBoxer1 + massBoxer2;
+        System.out.println(" Общая масса боксеров " + (massBoxer1 + massBoxer2) + " кг ");
+        var massDifference = massBoxer1 - massBoxer2;
+        System.out.println(" Разница между массами бойцов " + (massBoxer1 - massBoxer2) + " кг ");
+        var massDivision = massBoxer2 % massBoxer1;
+        System.out.println(massBoxer2 % massBoxer1);
+
+
+
+
+
+
+        var fullTime = 640;
+        var dayTime = 8;
+        var worker = fullTime / dayTime;
+        System.out.println(" Всего работников в компании " + worker + " человек ");
+        var addWorker = worker + 94;
+        var addFullTime = addWorker * dayTime;
+        System.out.println(" Если в компании работает " + addWorker + " человека, то всего " + addFullTime + " часов работы может быть поделено между сотрудниками");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
     }
 }
